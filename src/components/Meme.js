4f700c05ea762fe1ece0d0998c3978a6b57934fa
Jack Njoroge <jacknyange@fb.com>
2022-07-13 17:15:54 -0700

    layout + button functionality

---
+++ src/components/Meme.js
@@ -0,0 +1,34 @@
+import React from "react"
+import "../style.css"
+import memesData from "../memesData.js"
+
+function getMemeImage() {
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    console.log(url)
+}
+export default function Meme() {
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+        </main>
+    )
+}

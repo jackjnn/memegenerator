d3d372ab6be153c40af57daed534a1bd60794565
Jack Njoroge <jacknyange@fb.com>
2022-08-01 16:47:09 -0700

    Add process.env

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -62,7 +62,7 @@ export default function Meme() {
                 </button>
             </div>
             <div className="meme">
-                <img src={meme.randomImage} className="meme--image" />
+                <img src={ process.env.PUBLIC_URL + meme.randomImage} className="meme--image" />
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
